0ef957cc7682c4a9ba7edcc8fd3c960b37282e87
ngunga-dev <ngungadev@mail.com>
2022-04-28 05:09:46 +0100

    finalizando

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
           listLingua.push(linguas[lang]);
           setLingua(() => listLingua.map((langs) => langs));
         }
-        console.log(lingua);
+        console.log(listLingua);
       });
 
       console.log(country);
@@ -100,14 +100,17 @@ function App() {
   };
 
   return (
-    <div className="container-fluid">
-      <nav className="navbar navbar-expand-md navbar-primary bg-primary mb-5 ">
-        <div className="d-flex justify-content-between textAlign: center">
-          <h3 className="col-md-6 text-center text-white"></h3>
-        </div>
+    <div>
+      <nav className="navbar navbar-expand-sm bg-primary">
+          <div className="container">
+      <a className="navbar-brand" href="https://github.com/ngunga-dev/encotrarpais">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+      <small className="text-white"> GitHub</small>
+    </a>
+    </div>
       </nav>
 
-      <main className="container-fluid">
+      <main className="container">
         <div className="jumbotrom">
           <div className="col-md-6 offset-md-3 text-center">
             <h2>Conheça mais sobre os paises!</h2>
@@ -139,7 +142,7 @@ function App() {
             </div>
           </div>
 
-          <div className="container-fluid">
+          <div className="container">
             <div>
               {detalhe ? (
                 <div className="col-md-6 offset-md-3 text-center flex-wrap" display="flex" justifyContent="between">
@@ -160,8 +163,9 @@ function App() {
                 </div>
               ) : null}
             </div>
-            <div className="container-fluid row align-items-center justify-content-center">
-              <div className="col-6">
+            <div className="container">
+              <div className="row justify-content-around">
+              <div className="col-sm">
                 <div>
                   {detalhe ? (
                     <div className="lead">
@@ -188,7 +192,7 @@ function App() {
                       </p>
                       <p>
                         {" "}
-                        <strong></strong> Independecia :
+                        <strong></strong> Independência :
                         {detalhe.independencia ? "Sim" : "Não"}
                       </p>
                       <p>
@@ -205,7 +209,7 @@ function App() {
                 </div>
               </div>
 
-              <div className="col-6">
+              <div className="col-sm">
                 {weatherForecast ? (
                   <div>
                     <div className="mt-4 d-flex align-items-center">
@@ -244,19 +248,32 @@ function App() {
                 ) : null}
               </div>
 
-              <div>
+              </div>
+
+              <div className="container">
                 {detalhe ? (
-                  <div className="col-md-6 offset-md-3 text-center">
-                    <h2>Paises que fazem fronteira com {detalhe.name}</h2>
-                    <ul className="list-group ">
+                  <div className="row justify-content-around">
+                    <ul className="list-group col-sm">
+                    <h3
+                    className="text-center"
+                    >Fronteira com {detalhe.name}</h3>
                       {fronteiras.map((nome, index) => (
-                        <li className="list-group-item" key={index}>
+                        <li className="list-group-item lead" key={index}>
                           {nome}
                         </li>
                       ))}
                     </ul>
-                    <br></br>
-                    <br></br>
+
+                    <ul className="list-group col-sm">
+                      <h3
+                      className="text-center"
+                      >Linguas mais faladas {detalhe.name} </h3>
+                      {lingua.map((nome, index) => (
+                        <li className="list-group-item lead" key={index}>
+                          {nome}
+                        </li>
+                      ))}
+                    </ul>
                     <br></br>
                   </div>
                 ) : null}
@@ -266,9 +283,9 @@ function App() {
         </div>
       </main>
 
-      <footer className="navbar footer footer-fixed  navbar-primary bg-primary  content container-fluid">
+      {/* <footer className="navbar footer footer-fixed  navbar-primary bg-primary  content container">
         <p>Footer Text</p>
-      </footer>
+      </footer> */}
   
     </div>
   );
